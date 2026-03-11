abad42569f9ceb8158bdffbe4f8ab95d97bc183e
Jair Isaac <l21212024@tectijuana.edu.mx>
2026-03-11 15:33:08 -0700

    Construccion de la Fabrica

---
+++ App.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+#Fabrica abstracta
+class ComidaFactory(ABC):
+    @abstractmethod
+    def crear_comida(self):
+        pass
+
+
+class ChinaFactory(ComidaFactory):
+    def crear_comida(self):
+        return "Comida China creada"
+
+class JaponesFactory(ComidaFactory):
+    def crear_comida(self):
+        return "Comida Japonesa creada"
+
+class MexicanaFactory(ComidaFactory):
+    def crear_comida(self):
+        return "Comida Mexicana creada"
+
+class VietnamitaFactory(ComidaFactory):
+    def crear_comida(self):
+        return "Comida Vietnamita creada"
+
+class ItalianaFactory(ComidaFactory):
+    def crear_comida(self):
+        return "Comida Italiana creada"
+
+    
